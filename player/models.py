b9303fb29a65e98c92e993afadaa42a094d7634e
Cephlin <samjbren@gmail.com>
2015-10-23 12:33:30 +0100

    Fixed bugs for the pokemon and player models

--- player/models.py
+++ player/models.py
@@ -4,14 +4,16 @@ from pokemon.models import Pokemon
 
 class Player(models.Model):
     username = models.CharField(max_length=20)
-    team = models.ForeignKey(Pokemon)  # This is just a single pokemon until teams are added
-    usable_pokemon = len(team)
+    team = models.ForeignKey(Pokemon)  # This is just a single okemon until teams are added
+    usable_pokemon = 1 # Just a single Pokemon at the moment
 
     def get_and_update_usable_pokemon(self):
         usable_pokemon = 0
-        for pokemon in self.team:
-            if not pokemon.feinted:
-                usable_pokemon += 1
+        #for pokemon in self.team:
+        #    if not pokemon.feinted:
+        #        usable_pokemon += 1
+        if not self.team.fainted:
+            usable_pokemon += 1
 
         self.usable_pokemon = usable_pokemon
 
--- pokemon/models.py
+++ pokemon/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=50)
-    hp = models.IntegerField()
-    attack = models.IntegerField()
-    feinted = models.BooleanField(default=False)
+    hp = models.IntegerField(default=10)
+    attack = models.IntegerField(default=3)
+    fainted = models.BooleanField(default=False)
 
     def take_damage(self, damage):
         self.hp -= damage
         if self.hp <= 0:
-            feinted = True
+            self.fainted = True
 
     def __unicode__(self):
         return self.name
