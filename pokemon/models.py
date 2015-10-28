de3fffe4232242551dc4dfc3f610562749f5ebee
Cephlin <samjbren@gmail.com>
2015-10-28 00:27:47 +0000

    Added more to Pokemon model

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from type.models import Type
 
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=50)
+    type = models.ForeignKey(Type)
+
     hp = models.IntegerField(default=10)
     attack = models.IntegerField(default=3)
+    defence = models.IntegerField(defaults=10)
+    special_attack = models.IntegerField(default=10)
+    special_defence = models.IntegerField(default=10)
     speed = models.IntegerField(default=10)
+
     priority = models.IntegerField(default=1)
     fainted = models.BooleanField(default=False)
     base_pbv_value = models.IntegerField(default=23) # This would have an actual value of 115 PBV since we work in 5's
