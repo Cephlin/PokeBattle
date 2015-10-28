10571ce67f4b8015c160da32cadd070881e15caa
Cephlin <samjbren@gmail.com>
2015-10-28 00:48:21 +0000

    Updated genx tests

--- genx/tests.py
+++ genx/tests.py
@@ -7,16 +7,20 @@ from battle.models import Battle
 
 
 class TestType(TestCase):
-    @patch('genx.models.Gen', return_value=1)
+    #@patch('genx.models.Gen', return_value=1)
     def test_get_damage(self):
-        red_level = 1
-        red_attack = 1
-        blue_defence = 1
-        red_move_base = 1
-        red_modifier = Gen.get_modifier()
+        red_level = 5
+        red_attack = 10
+        blue_defence = 10
+        red_move_base = 50
+        red_modifier = 1
         damage = Gen.get_damage(self, red_level, red_attack, blue_defence, red_move_base, red_modifier)
-        self.assertEqual(damage, 50)
 
+        # (((((2 * level) + 10) / 250) * (attack / defence) * base) + 2) * modifier
+        # (((((2 * 5) + 10) / 250) * (10 / 10) * 50) + 2) * 1 = ((2 /25) * 50) + 2 = 4
+        self.assertEqual(damage, 4)
+
+    """
     def do_setup(self, red_attack, red_speed, blue_attack, blue_speed):
         self.red_pokemon = Pokemon(name='Charmander', attack=red_attack, speed=red_speed)
         self.red_pokemon.save()
@@ -28,3 +32,4 @@ class TestType(TestCase):
         self.blue.save()
         self.battle = Battle(red=self.red, blue=self.blue)
         self.battle.save()
+    """
